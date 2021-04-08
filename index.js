e99abf9d850856277a3236c67241d6949f10e170
Suhair <saljammal995@gmail.com>
2021-04-08 14:35:32 +0200

    commit first version of my Portfolio using HTML, CSS and Vanilla Javascript

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// window.addEventListener("resize", function(){
+//     if(window.innerWidth <= 800){
+//         const icons = document.querySelectorAll("i");
+//         // icons[0].classList.remove("fa-2x");
+//         icons[1].classList.remove("fa-2x");
+//         icons[2].classList.remove("fa-2x");
+
+
+//     }
+// });
+
+
+
+
+function myFacebook() {
+    window.open("https://www.facebook.com/soso.aljammal.9")
+  }
+  function myGithub(){
+      window.open("https://github.com/Suha995");
+  }
+  function myLinkedin(){
+      window.open("https://www.linkedin.com/in/suhair-aljammal");
+  }
+  function addBurger(){
+      const menu = document.querySelector(".menu");
+      menu.classList.toggle("menu-active");    
+  }
+
