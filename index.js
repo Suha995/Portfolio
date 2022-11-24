a95b9255856b4dd1040bebdf8e89554aa67f2d9f
Suhair <saljammal995@gmail.com>
2022-11-24 17:28:37 +0200

    add some changes

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
+const firstname = document.getElementById("fn");
+const lastname = document.getElementById("ln");
+const email = document.getElementById("email");
+const msg = document.getElementById("msg");
+const submitForm = document.querySelector(".contact-container");
 
-// window.addEventListener("resize", function(){
-//     if(window.innerWidth <= 800){
-//         const icons = document.querySelectorAll("i");
-//         // icons[0].classList.remove("fa-2x");
-//         icons[1].classList.remove("fa-2x");
-//         icons[2].classList.remove("fa-2x");
-
-
-//     }
-// });
-
-
-
+submitForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("clicked");
+});
 
 function myFacebook() {
-    window.open("https://www.facebook.com/soso.aljammal.9")
-  }
-  function myGithub(){
-      window.open("https://github.com/Suha995");
-  }
-  function myLinkedin(){
-      window.open("https://www.linkedin.com/in/suhair-aljammal");
-  }
-  function addBurger(){
-      const menu = document.querySelector(".menu");
-      menu.classList.toggle("menu-active");    
-  }
+  window.open("https://www.facebook.com/soso.aljammal.9");
+}
+function myGithub() {
+  window.open("https://github.com/Suha995");
+}
+function myLinkedin() {
+  window.open("https://www.linkedin.com/in/suhair-aljammal");
+}
+function addBurger() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("menu-active");
+}
 
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "username",
+    Password: "password",
+    To: "them@website.com",
+    From: "you@isp.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
